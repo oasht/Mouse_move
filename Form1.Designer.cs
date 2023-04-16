bf555303142cd4c4cee9475437c843cbba426bc9
Olga Shtoppel <olgashtoppel@gmail.com>
2023-04-16 16:12:14 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Mouse_move
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(357, 134);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(787, 622);
+            this.label1.TabIndex = 0;
+            this.label1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.label1_MouseClick);
+            this.label1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label1_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(1495, 914);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
+namespace Mouse_move
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string Coord_mouse(MouseEventArgs e)
+        {
+            return $"Coordinates: x= {e.X.ToString()}; y={e.Y.ToString()}";
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Text = Coord_mouse(e);
+            
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+       
+                string mes = "Вы нажали на внешнюю область";
+                mes += $"\n {Coord_mouse(e)}";
+                string caption = "Mouse click";
+                MessageBox.Show(mes, caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            
+        }
+
+        private void label1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+            if (e.X == 0 || e.Y == 0|| e.Y == 321|| e.X == 392)
+            {
+                string mes = "Вы нажали на границу внутренней области";
+                mes += $"\n {Coord_mouse(e)}";
+                string caption = "Mouse click";
+                MessageBox.Show(mes, caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                string mes = "Вы нажали на внутреннюю область";
+                mes += $"\n {Coord_mouse(e)}";
+                string caption = "Mouse click";
+                MessageBox.Show(mes, caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            
+        }
+
+        private void label1_MouseMove(object sender, MouseEventArgs e)
+        {
+            
+            Text = Coord_mouse(e);
+        }
+    }
+}
